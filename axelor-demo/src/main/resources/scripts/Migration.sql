0b6e78ec2ca4eef67f3d4afa8f63aa9c94f65aba
HORRI Khalid <horrikhalid@gmail.com>
2018-04-20 12:37:58 +0100

    Fiche agent

--- axelor-demo/src/main/resources/scripts/Migration.sql
+++ axelor-demo/src/main/resources/scripts/Migration.sql
@@ -9,10 +9,12 @@ gesper.config_filiere.id as filiere
 FROM
 grh_2208.grades
 left Join gesper.config_filiere ON grh_2208.grades.FIL_COD = gesper.config_filiere.code
-left Join gesper.config_cadre ON grh_2208.grades.CAD_COD = gesper.config_cadre.code
+left Join gesper.config_cadre ON grh_2208.grades.CAD_COD = gesper.config_cadre.code;
 --------------------------------------------------------------------------------------------------------
 -------------------- GRADE  ECHELON MIGRATION ------------------------------------
-INSERT INTO gesper.config_grade_echelon (echelon,indice,rythme_lent,rythme_moyen,rythme_rapide,grade) SELECT
+set @id:=1;
+INSERT INTO gesper.config_grade_echelon (id,echelon,indice,rythme_lent,rythme_moyen,rythme_rapide,grade) SELECT
+(@id:=@id+1),
 grh_2208.grille_indiciaire.ECHE_COD,
 grh_2208.grille_indiciaire.GRII_IND,
 grh_2208.echelons_grade.EPG_LEN,
@@ -22,5 +24,4 @@ grh_2208.echelons_grade.GRA_COD
 FROM
 grh_2208.grille_indiciaire
 LEFT Join grh_2208.echelons_grade ON grh_2208.grille_indiciaire.GRA_COD = grh_2208.echelons_grade.GRA_COD AND grh_2208.grille_indiciaire.ECHE_COD = grh_2208.echelons_grade.ECHE_COD
-
 
