86a58ebfaee88c714a9bd97eda194126fd1bb95a
AYOUB EL ABASSI <a.elabassi@handiis.com>
2018-07-11 11:12:37 +0100

    Note module

--- axelor-demo/modules/gesper-rh/src/main/java/com/axelor/rh/service/EmployeService.java
+++ axelor-demo/modules/gesper-rh/src/main/java/com/axelor/rh/service/EmployeService.java
@@ -3,6 +3,7 @@ package com.axelor.rh.service;
 import com.axelor.apps.ReportFactory;
 import com.axelor.exception.AxelorException;
 import com.axelor.i18n.I18n;
+import com.axelor.report.ReportGenerator;
 import com.axelor.rh.db.Employe;
 import com.axelor.rh.db.repo.EmployeRepository;
 import com.axelor.rh.report.IReport;
@@ -10,8 +11,10 @@ import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.Serializable;
+import java.io.*;
 import java.lang.invoke.MethodHandles;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * Created by HORRI on 22/05/2018.
@@ -22,6 +25,9 @@ public class EmployeService implements Serializable{
 
     @Inject
     private EmployeRepository employeRepository;
+
+    @Inject
+    private ReportGenerator generator;
 
     public String getFileName(Employe agent) {
         return I18n.get("work certificate") + " " + agent.getMatricule() ;
@@ -35,5 +41,15 @@ public class EmployeService implements Serializable{
                 .addFormat(format)
                 .generate()
                 .getFileLink();
+    }
+
+    public void print(String design, Map<String, Object> params) throws IOException {
+
+        OutputStream output = new FileOutputStream(new File(""));
+        try {
+            //generator.generate(output, design, params, Locale.getDefault());
+        } finally {
+            output.close();
+        }
     }
 }
