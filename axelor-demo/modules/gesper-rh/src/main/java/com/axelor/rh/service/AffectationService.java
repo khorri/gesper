d0feadd3a00e480b3ae463085aa50c6a131634c4
noredine0 <noredine.o3o@gmail.com>
2018-07-19 18:09:08 +0100

    Affectation

---
+++ axelor-demo/modules/gesper-rh/src/main/java/com/axelor/rh/service/AffectationService.java
@@ -0,0 +1,48 @@
+package com.axelor.rh.service;
+
+import com.axelor.apps.ReportFactory;
+import com.axelor.exception.AxelorException;
+import com.axelor.i18n.I18n;
+import com.axelor.report.ReportGenerator;
+import com.axelor.rh.db.Employe;
+import com.axelor.rh.db.repo.EmployeRepository;
+import com.axelor.rh.report.IReport;
+import com.google.inject.Inject;
+import org.eclipse.birt.report.engine.api.IReportEngine;
+import org.eclipse.birt.report.model.api.IResourceLocator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.lang.invoke.MethodHandles;
+import java.net.URL;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * Created by HORRI on 22/05/2018.
+ */
+public class EmployeService implements Serializable{
+
+    private final Logger logger = LoggerFactory.getLogger( MethodHandles.lookup().lookupClass() );
+
+    @Inject
+    private EmployeRepository employeRepository;
+
+    @Inject
+    private ReportGenerator generator;
+    
+    public String getFileName(Employe agent) {
+        return I18n.get("work certificate") + " " + agent.getMatricule() ;
+    }
+
+    public String getReportLink(Employe agent, String name, String language, String format) throws AxelorException {
+
+        return ReportFactory.createReport(IReport.WORK_CERTIFICATE, name+"-${date}")
+                .addParam("Locale", language)
+                .addParam("SaleOrderId", agent.getId())
+                .addFormat(format)
+                .generate()
+                .getFileLink();
+    }
+}
